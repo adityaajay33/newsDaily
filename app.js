9fbe4df4061b86825ed9ac5005b2700bf372cfed
adityaajay33 <107756503+adityaajay33@users.noreply.github.com>
2024-01-03 19:08:25 -0500

    added functionality from New York Times API

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require('express');
 require('dotenv').config()
+const axios = require('axios');
 
 const PORT = process.env.PORT || 3001;
+const API_KEY = process.env.NYT_API_KEY;
 
 const app = express();
 
@@ -10,3 +12,30 @@ app.use(express.json());
 app.listen(PORT, () => {
     console.log(`Server Started at ${PORT}`)
 })
+
+const apiUrl = `https://api.nytimes.com/svc/mostpopular/v2/viewed/1.json?api-key=${API_KEY}`;
+
+console.log(apiUrl);
+
+
+
+// Make a GET request
+axios.get(apiUrl)
+    .then(response => {
+        // Handle the response data
+
+        const data = response.data.results;
+
+        for(let i=0; i<data.length; i++){
+
+            console.log(data[i].title);
+        }
+
+    })
+    .catch(error => {
+        // Handle errors
+        console.error('Error:', error);
+    });
+
+
+   
