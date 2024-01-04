cab2ed251bac4caabc58e487d4ea4655ebdf7519
adityaajay33 <107756503+adityaajay33@users.noreply.github.com>
2024-01-03 20:28:07 -0500

    set up api requests

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 require('dotenv').config()
 const axios = require('axios');
+const { sendMessage } = require('./sms');
 
 const PORT = process.env.PORT || 3001;
 const API_KEY = process.env.NYT_API_KEY;
@@ -15,25 +16,16 @@ app.listen(PORT, () => {
 
 const apiUrl = `https://api.nytimes.com/svc/mostpopular/v2/viewed/1.json?api-key=${API_KEY}`;
 
-console.log(apiUrl);
 
-
-
-// Make a GET request
 axios.get(apiUrl)
     .then(response => {
-        // Handle the response data
-
+        
         const data = response.data.results;
-
         for(let i=0; i<data.length; i++){
-
             console.log(data[i].title);
         }
-
     })
     .catch(error => {
-        // Handle errors
         console.error('Error:', error);
     });
 
